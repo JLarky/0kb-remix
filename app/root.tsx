67511f8f5fceb93c32bf443218bb1fe07ae418fb
JLarky <jlarky@gmail.com>
2022-02-13 21:34:54 -0700

    0kb remix with partytown
    - we don't actually run remix runtime inside web worker, that would be too slow
    - we just make website look good by including js later
    - breaks all the time

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,38 @@ export default function App() {
       <body>
         <Outlet />
         <ScrollRestoration />
-        {/* <Scripts /> */}
+        <noscript id="_remix">
+          <Scripts />
+        </noscript>
+        <script
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `
+const start = async () => {
+  const div = document.createElement("div"); div.innerHTML = document.querySelector('#_remix').firstChild.textContent;
+
+  var s = div.firstChild;
+  while (s) {
+    s = s.nextSibling;
+    if (s && s.nodeName === 'SCRIPT') {
+      var t = document.createElement("script"); t.textContent = s.textContent; s.src && (t.src = s.src); t.type = s.type; t;
+      document.getElementsByTagName('head')[0].appendChild(t);
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+  }
+  console.log('finished!');
+}
+const x = document.createElement('div');
+console.log('partytown!', x);
+x.textContent = '🎉';
+x.style.position = 'fixed';
+x.style.bottom = '0';
+x.style.right = '0';
+document.getElementsByTagName('body')[0].appendChild(x);
+start();
+            `,
+          }}
+        />
         {process.env.NODE_ENV === "development" && <LiveReload />}
       </body>
     </html>
