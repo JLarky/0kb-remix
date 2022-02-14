2b2ca7feb3dbbec04f9d5e5ef917aeaaa40f75ef
JLarky <jlarky@gmail.com>
2022-02-13 19:51:15 -0700

    npx create-remix@latest

---
+++ app/routes/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function Index() {
+  const [pos, setPos] = React.useState("");
+  React.useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      setPos(`${clientX} ${clientY}`);
+    });
+  }, []);
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix {pos}</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
